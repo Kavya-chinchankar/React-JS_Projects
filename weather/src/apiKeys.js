6916abfad8a188dee7e859678fada12fa731017e
Kavya Krishna chinchankar <kavyachinchankar@gmail.com>
2024-04-28 15:38:37 +0530

    first commit

---
+++ weather/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0939504b72fb7c3be0cf0eb3b3e244ed",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
